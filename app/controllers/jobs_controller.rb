b901980395033cb67a42d72558089873bc6382dd
milkfilk <milkfilk@gmail.com>
2011-02-27 15:33:33 -0500

    Cleanup.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,14 +9,15 @@ class JobsController < ApplicationController
   def new
     job_name = params[:id]
     
-    
+    # goddamn singleton won't work with delayed job, diff ruby VMs
+    # we only want one scraper running at a time, lest we get google banned
     if Jobstates.find_by_name("counter").nil?
       Jobstates.create(:name => "counter", :started => Date.new, :running => true)
       @counter = Counter.new
       @counter.delay.count
       flash[:notice] = "Created job: #{job_name}"
     else
-      flash[:alert] = "Sorry.  Job #{job_name} is already running."
+      flash[:alert] = "Job #{job_name} is already running."
     end
     
     #@counter = Delayed::Job.enqueue Paranoid.new
--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -2,6 +2,6 @@
 Delayed::Worker.backend = :active_record
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 5
-Delayed::Worker.max_attempts = 10
-Delayed::Worker.max_run_time = 1.minutes
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 300.minutes
 #Delayed::Worker.delay_jobs = !Rails.env.test?
