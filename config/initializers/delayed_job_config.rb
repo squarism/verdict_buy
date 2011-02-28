0c00cc9e7aa13978e113ebc5ed7788c042650dd4
milkfilk <milkfilk@gmail.com>
2011-02-28 10:08:50 -0500

    Dev settings for DJ.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -2,6 +2,6 @@
 Delayed::Worker.backend = :active_record
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 5
-Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_attempts = 1
 Delayed::Worker.max_run_time = 300.minutes
 #Delayed::Worker.delay_jobs = !Rails.env.test?
