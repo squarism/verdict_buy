f9188f4846882d97c73907ba9d896d92202b18f3
milkfilk <milkfilk@gmail.com>
2011-02-28 11:11:34 -0500

    Single ars_title for top percentage appearing in title array helper added.

--- app/models/ars_review.rb
+++ app/models/ars_review.rb
@@ -2,4 +2,10 @@ class ArsReview < ActiveRecord::Base
   has_many :ars_titles
   has_many :shop_list_items
   has_one :love
+  
+  # most likely title
+  # TODO: need to trim out junk data that makes the title distribution 50/50
+  def ars_title
+    self.ars_titles.order('percent_appears desc').limit(1)
+  end
 end
--- app/models/love.rb
+++ app/models/love.rb
@@ -1,3 +1,4 @@
 class Love < ActiveRecord::Base
   belongs_to :ars_review
+  has_many :ars_titles, :through => :ars_review
 end
