b2f00ac644d694fb848c2bf3e5a109f1e455377f
milkfilk <milkfilk@gmail.com>
2011-03-04 12:12:16 -0500

    Update all artwork.  Added scope.

--- app/jobs/artwork.rb
+++ app/jobs/artwork.rb
@@ -1,7 +1,7 @@
 class Artwork
   
   def update_all
-    
+    Love.all_with_gbid.each {|love| a.update(love.gb_id)}
   end
   
   def update(id)
@@ -11,18 +11,11 @@ class Artwork
     
     self.save_image(thumb_image, "#{id}_thumb", "public/images/art_cache")
     self.save_image(super_image, "#{id}_super", "public/images/art_cache")
-    
   end
 
-
   def save_image(url, base_name, directory)
-    # Parse the image name out of the url. We'll use that
-    # name to save it down.
-    #file_name = parse_file_name(url)
-    
     # keep the remote extension
     file_extension = parse_extension(url)
-    
     file_name = "#{base_name}#{file_extension}"
 
     if File.exist?(directory + "/" + file_name)
@@ -31,8 +24,7 @@ class Artwork
       # Get the response and data from the web for this image.
       response = Net::HTTP.get_response(URI.parse(url))
 
-      # If the response is not nil, save the contents down to
-      # an image.
+      # If the response is not nil, save the contents down to an image.
       if !response.nil?
         f = File.open(directory + "/" + file_name, "wb+")
         f << response.body
@@ -44,8 +36,7 @@ class Artwork
   end
   
   def parse_file_name(url)
-    # Find the position of the last slash. Everything after
-    # it is our file name.
+    # Find the position of the last slash. Everything after it is our file name.
     spos = url.rindex("/")
     url[spos + 1, url.length - 1]
   end
@@ -53,8 +44,6 @@ class Artwork
   def parse_extension(url)
     url[url.rindex("."), url.length - 1]
   end
-  
-  
   
 end
 
--- app/models/love.rb
+++ app/models/love.rb
@@ -2,9 +2,13 @@ class Love < ActiveRecord::Base
   belongs_to :ars_review
   has_many :ars_titles, :through => :ars_review
   
-  # example: Love.all_validated.collect(&:gb_title)
+  # example: Love.all_validated.collect(&:gb_title) 
   scope :all_validated, lambda {
     find(:all, :conditions => [ 'gb_title <> "<<NO GB HIT>>"' ] )
+  }
+
+  scope :all_with_gbid, lambda {
+    find(:all, :conditions => [ 'gb_id IS NOT NULL' ] )
   }
   
 end
