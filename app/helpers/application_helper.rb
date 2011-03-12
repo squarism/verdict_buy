8294f625682f591e9d9bfb8ee545f409d0d6203f
milkfilk <milkfilk@gmail.com>
2011-03-11 19:31:15 -0500

    Art thumb rework, clean up on lookup.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,19 +15,31 @@ module ApplicationHelper
   # get an image with a _icon _super _whatever bit of text in it
   # if no file is there, return a placeholder that follows this naming
   # convention.
-  def art_path(id, underscore_name)
+  def art_path(love, underscore_name)
     # TODO: adding other file types, this might break
     # extensions = [ ".jpg", ".png", ".jpeg", ".gif" ]
 
+    #love = Love.find(id)
+
     # match any file in the image directory
-    wildcard = "public/images/art_cache/#{id}_#{underscore_name}*"
+    wildcard = "public/images/art_cache/#{love.gb_id}_#{underscore_name}*"
     # we shouldn't have multiple files extensions for the same game so just grab first
     file_match = Dir[wildcard].first
+    
+    # alt tag text bit
+    # love_title = love.gb
+    # love_title = Love.where(:gb_id => love.gb_id).first
+    if !love.gb_title = "<<NO GB HIT>>"
+      alt_tag = love.gb_title
+    else
+      alt_tag = love.ars_review.ars_title
+    end
+    puts alt_tag
 
     if file_match
-      return image_tag file_match.sub(/^public\/images\//,'')
+      return image_tag file_match.sub(/^public\/images\//,''), :title => alt_tag
     else
-      return image_tag "/images/no_art_#{underscore_name}.gif"
+      return image_tag "/images/no_art_#{underscore_name}.gif", :title => alt_tag
     end
   end
   
--- app/jobs/giant_lookup.rb
+++ app/jobs/giant_lookup.rb
@@ -58,40 +58,98 @@ class GiantLookup
   
   def find_titles(title)
     results = @client.find_game(title)
-    results.collect(&:name)
+    return results.collect(&:name) unless results.nil?
+    
+    # try with no parens: Blue Dragon (Xbox 360)
+    title_without_parens = title.scan(/(.*)(\(.*\))/).flatten.first.strip
+    results = @client.find_game(title_without_parens)
+    return results.collect(&:name) unless results.nil?
+    
+    # try with no commas: Blue Dragon (Xbox 360),
+    results = @client.find_game(title_without_parens.gsub(",", ""))
+    return results.collect(&:name) unless results.nil?
+    
+    #results.collect(&:name)
   end
   
   def update_love
     
     ArsReview.all.each do |review|
       title = review.ars_title
+      puts title
       
       # if a custom title is set, use that instead of the ars title
       if !review.love.title.nil?
         title = review.love.title
       end
-
-      gb_titles = self.find_games_by_title(title)
-
-      # Do we have at least one hit from Giant Bomb?
-      if gb_titles.map{|g| g[:name].downcase}.include?(title.downcase)
-        # find a title using downcase.  if we have multiple hits, it's probably multiple platforms
-        # so just pick the first one
-        match = gb_titles.select {|hash| hash[:name].downcase == title.downcase}.first
-
-        review.love.gb_title = match[:name]
-        review.love.gb_id = match[:id]
+      
+      # check for exact matches first
+      exact_match = try_exact_match review
+      if exact_match
+        set_love(review, exact_match)
       else
-        review.love.gb_title = "<<NO GB HIT>>"
+        match = handle_fifty_fifty review
+        if match
+          set_love(review, match)
+        else
+          review.love.gb_title = "<<NO GB HIT>>"
+        end
       end
+
+      # 
+      #   review.love.gb_title = match[:name]
+      #   review.love.gb_id = match[:id]
+      # else
+      #   review.love.gb_title = "<<NO GB HIT>>"
+      # end
       
       # only set to defaults if is null in DB, otherwise, it will overwrite own/ignore each run
-      review.love.owned = false if review.love.owned.nil?
-      review.love.ignored = false if review.love.ignored.nil?
-
-      review.love.save
     end
     
+  end
+  
+  # return a GBID from an array of titles.  any we have an exact title?
+  def try_exact_match(review)
+    possible_titles = review.ars_titles.collect(&:title)
+    exact_match = false
+    possible_titles.each do |possible_title|
+      puts "TRYING #{possible_title} for exact match"
+      if GiantLookup.new.is_title? possible_title
+        return GiantLookup.new.find_games_by_title(possible_title).first
+      end
+    end
+    
+    # nothing found, oh well
+    nil
+  end
+  
+  # our percentage guess might be 50/50
+  def handle_fifty_fifty(review)
+    title = review.ars_title
+    gb_titles = self.find_games_by_title(title)
+
+    # Do we have at least one hit from Giant Bomb?
+    if gb_titles.map{|g| g[:name].downcase}.include?(title.downcase)
+      # find a title using downcase.  if we have multiple hits, it's probably multiple platforms
+      # so just pick the first one
+      match = gb_titles.select {|hash| hash[:name].downcase == title.downcase}.first
+    end
+  end
+  
+  # update our love cache table
+  def set_love(review, hash)
+    review.love.gb_title = hash[:name]
+    review.love.gb_id = hash[:id]
+    # TODO: cache attributes:
+    # platforms
+    # release date
+    # developers
+    # publishers
+    # game rating
+    # description
+    review.love.owned = false if review.love.owned.nil?
+    review.love.ignored = false if review.love.ignored.nil?
+    review.love.save
   end
   
   def after
